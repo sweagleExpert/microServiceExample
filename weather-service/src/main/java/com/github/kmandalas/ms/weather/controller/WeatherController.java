125437d65838cfc3ed060adb9f3fcf8e33c01822
k.mandalas <kyriakos@sweagle.expert>
2018-04-03 16:59:32 +0300

    added @RefreshScope

--- weather-service/src/main/java/com/github/kmandalas/ms/weather/controller/WeatherController.java
+++ weather-service/src/main/java/com/github/kmandalas/ms/weather/controller/WeatherController.java
@@ -1,6 +1,7 @@
 package com.github.kmandalas.ms.weather.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController("/weather")
 @Slf4j
+@RefreshScope
 public class WeatherController {
 
 	@Value("${default-city}")
